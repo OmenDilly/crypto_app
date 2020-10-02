d6c1136202464e3a38bb516d3e50a232c91307d3
OmenDilly <blokkb@mail.ru>
2020-10-02 21:44:57 +0700

    initialization

---
+++ components/Nav.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="nav__container">
+		<span class="logo">
+			<img src="" alt="">
+		</span>
+		<div class="links">
+			<span class="main__button">
+				Главная
+			</span>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='sass'>
+	.nav__container
+		background-color: $backgroundPrimary
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="main__container">
+
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang='sass'>
+  .main__container
+    display: grid
+</style>
